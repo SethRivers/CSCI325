98d2eff9290cf71cd5c0ddc6d7e7575235cef66a
Seth Rivers <davmrive@ut.utm.edu>
2024-09-25 00:17:08 -0500

    Filled out .h file. mostly without comments

--- HW_1/Vector.h
+++ HW_1/Vector.h
@@ -7,3 +7,31 @@
  * 
  */
 
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector{
+ private:
+  int *vec_ptr;
+  int vec_size, vec_capacity;
+
+ public:
+  Vector();
+
+  Vector(const Vector &other);
+
+  Vector& operator=(const Vector &other);
+
+  int size();
+
+  int capacity();
+
+  void push_back(int element);
+
+  void reserve(int n);
+
+  int& operator[](unsigned int index);
+  
+  ~Vector();
+
+#endif //VECTOR_H
